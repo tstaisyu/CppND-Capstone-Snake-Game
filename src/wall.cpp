3057da638526734d6d6a36aabb28da700b3cc6ed
taisyu shibata <t_shiba117@yahoo.co.jp>
2023-07-10 18:50:10 +0900

    Edit Wall class

--- src/wall.cpp
+++ src/wall.cpp
@@ -5,20 +5,6 @@
 #include "game.h"
 
 void Snake::Update() {
-  SDL_Point prev_cell{
-      static_cast<int>(head_x),
-      static_cast<int>(
-          head_y)};  // We first capture the head's cell before updating.
-  UpdateHead();
-  SDL_Point current_cell{
-      static_cast<int>(head_x),
-      static_cast<int>(head_y)};  // Capture the head's cell after updating.
-
-  // Update all of the body vector items if the snake head has moved to a new
-  // cell.
-  if (current_cell.x != prev_cell.x || current_cell.y != prev_cell.y) {
-    UpdateBody(current_cell, prev_cell);
-  }
 }
 
 void Snake::UpdateHead() {
@@ -46,34 +32,7 @@ void Snake::UpdateHead() {
 }
 
 void Snake::UpdateBody(SDL_Point &current_head_cell, SDL_Point &prev_head_cell) {
-  // Add previous head location to vector
-  body.push_back(prev_head_cell);
-
-  if (!growing) {
-    // Remove the tail from the vector.
-    body.erase(body.begin());
-  } else {
-    growing = false;
-    size++;
-  }
-
-  if (lostBody) {
-    // Remove the tail from the vector.
-    body.clear();
-    lostBody = false;
-    size = 1;
-  }
-
-  // Check if the snake has died.
-  for (auto const &item : body) {
-    if (current_head_cell.x == item.x && current_head_cell.y == item.y) {
-     // alive = false;
-    }
-  }
 }
-
-void Snake::GrowBody() { growing = true; }
-void Snake::LostBody() { lostBody = true; }
 
 void Snake::WallRedirect() {
   if (Game::_Wall == true) {
@@ -96,8 +55,6 @@ void Snake::WallRedirect() {
     }
   }
 }
-
-
 
 // Inefficient method to check if cell is occupied by snake.
 bool Snake::SnakeCell(int x, int y) {
