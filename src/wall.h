54c35c899ddc31f0bb947747fb86bcc3b7aac12c
Taisyu Shibata <t_shiba117@yahoo.co.jp>
2023-07-10 16:42:02 +0900

    Add wall.h/wall.cpp

---
+++ src/wall.h
@@ -0,0 +1,43 @@
+#ifndef WALL_H
+#define WALL_H
+
+#include <vector>
+#include "SDL.h"
+
+class Snake {
+ public:
+  enum class Direction { kUp, kDown, kLeft, kRight };
+
+  Snake(int grid_width, int grid_height)
+      : grid_width(grid_width),
+        grid_height(grid_height),
+        head_x(grid_width / 2),
+        head_y(grid_height / 2) {}
+
+  void Update();
+
+  void GrowBody();
+  void LostBody();
+  void WallRedirect();
+  bool SnakeCell(int x, int y);
+
+  Direction direction = Direction::kUp;
+
+  float speed{0.2f};
+  int size{1};
+  bool alive{true};
+  float head_x;
+  float head_y;
+  std::vector<SDL_Point> body;
+
+ private:
+  void UpdateHead();
+  void UpdateBody(SDL_Point &current_cell, SDL_Point &prev_cell);
+
+  bool growing{false};
+  bool lostBody{false};
+  int grid_width;
+  int grid_height;
+};
+
+#endif
